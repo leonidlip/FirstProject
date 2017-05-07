050d216191310a824a9645c3c15bdd7ade9de4e5
leonidlip <lipmail@gmail.com>
2017-05-07 16:46:42 +0300

    Update 07 grant to IMAGE DQ.sql

--- 07 grant to IMAGE DQ.sql
+++ 07 grant to IMAGE DQ.sql
@@ -1,3 +1,3 @@
 -- Grant/Revoke object privileges 
 grant select, insert, update, delete, references, read on ANGBORD_MAIN.IMAGE to migration;
-grant select, insert, update, delete, references, read on Verification_Result to migration;
+grant select, insert, update, delete, references, read on ANGBORD_MAIN.Verification_Result to migration;
