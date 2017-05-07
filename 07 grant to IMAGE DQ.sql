71c0975cc665d4c27a18289fea01f9f8191250e5
leonidlip <lipmail@gmail.com>
2017-05-07 16:44:32 +0300

    Add files via upload

---
+++ 07 grant to IMAGE DQ.sql
@@ -0,0 +1,2 @@
+-- Grant/Revoke object privileges 
+grant select, insert, update, delete, references, read on ANGBORD_MAIN.IMAGE to migration;
---
+++ 08 Verification_result alter D Q .sql
@@ -0,0 +1,1 @@
+alter table VERIFICATION_RESULT_MAG add loaded_to_target NUMBER(1) default 0;
